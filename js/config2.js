24924c11c34fa943727258e0e219237aad12cd99
IsmaelRodRam <u1988495@campus.udg.edu>
2024-06-20 18:00:48 +0200

    añadido config2

---
+++ js/config2.js
@@ -0,0 +1,19 @@
+import { PlayScene } from "./scene2.js";
+
+var config = {
+    type: Phaser.AUTO,
+    domCreateContainer: true,
+    width: 800,
+    height: 600,
+    parent: 'game',
+    scene: [PlayScene],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 0},
+            debug: false
+        }
+    }
+}
+
+var game = new Phaser.Game(config);
